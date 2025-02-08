7cd9bb7d616df87cd476d3685481afb34d80d023
yuzili <395345203@qq.com>
2025-02-08 15:44:15 +0200

    feat: resolve package.

--- packages/node-modules-tools/src/resolve.ts
+++ packages/node-modules-tools/src/resolve.ts
@@ -1,4 +1,46 @@
 /**
  * Analyze a package node, and return a resolved package node.
  * This function mutates the input package node.
+ *
+ * - Set `module` to the resolved module type (cjs, esm, dual, faux, none).
  */
+
+import type { PackageJson } from 'pkg-types'
+import type { BaseOptions, PackageNode, PackageNodeBase } from './types'
+
+import fse from 'node:fs/promises'
+import { join } from 'node:path'
+import { analyzePackageModuleType } from './analyze-esm'
+import { getPackagesInstallSize } from './size'
+import { stripBomTag } from './utils'
+
+export async function resolvePackage(_packageManager: string, pkg: PackageNodeBase, _options: BaseOptions): Promise<PackageNode> {
+  const _pkg = pkg as unknown as PackageNode
+  if (_pkg.resolved)
+    return _pkg
+
+  const content = await fse.readFile(join(pkg.filePath, 'package.json'), 'utf-8')
+  const json = JSON.parse(stripBomTag(content)) as PackageJson
+
+  let repository = typeof json.repository === 'string' ? json.repository : json.repository?.url
+  if (repository?.startsWith('git+'))
+    repository = repository.slice(4)
+  if (repository?.endsWith('.git'))
+    repository = repository.slice(0, -4)
+  if (repository?.startsWith('git://'))
+    repository = repository.slice(6)
+  if (json.repository && typeof json.repository !== 'string' && json.repository.directory)
+    repository += `/tree/HEAD/${json.repository.directory}`
+
+  _pkg.resolved = {
+    module: analyzePackageModuleType(json),
+    engines: json.engines,
+    license: json.license,
+    author: typeof json.author === 'string' ? json.author : json.author?.url,
+    repository,
+    homepage: json.homepage,
+    installSize: await getPackagesInstallSize(_pkg),
+  }
+
+  return _pkg
+}
---
+++ packages/node-modules-tools/src/utils.ts
@@ -0,0 +1,7 @@
+// strip UTF-8 BOM
+export function stripBomTag(content: string): string {
+  // @ts-expect-error unicorn/number-literal-case
+  if (content.charAt(0) === 0xFEFF)
+    return content.slice(1)
+  return content
+}
